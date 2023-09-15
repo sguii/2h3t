3d5b58c002b0f41c58fd8ac46c91eeeb10f7d7a6
guilherme <105298617+sguii@users.noreply.github.com>
2023-09-15 08:10:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 clicado = () => alert ("Alô mundo")
+mensagem = (msg) => console.log(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+juros_simples = (c,j) => mult (c,div(j,100));
+montante_juros_simples = (c,j) => soma(juros_simples(c,j),c);
+
+
+
+mensagem(soma(15,soma(11,soma(4,7))));
+mensagem(sub(18,7));
+mensagem(soma(2,div(2,2)));
+
+
+mensagem(juros_simples(420,18))
+mensagem(montante_juros_simples(420,18));
