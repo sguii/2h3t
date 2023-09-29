ba56632e459d65d4cb5784fa4ab6dc8844303faf
guilherme <105298617+sguii@users.noreply.github.com>
2023-09-29 08:09:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-clicado = () => alert ("Alô mundo")
+
 mensagem = (msg) => console.log(msg);
 soma = (a,b) => a + b;
 sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
+juros_simples = (c,j) => mult(c,div(j,100));
+montante_juro_simples = (c,j) => soma(juros_simples(c,j),c);
 
-
+mostrar_display = (msg)=> {
+    document.getElementById("resultado").value = msg;
+}
+function digitando(tecla) {
+if (tecla == "."){
+    if
+}
+    valor += tecla;
+mostrar_display(valor);
+}
